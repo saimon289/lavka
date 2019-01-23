badbd1918c7111d0eab88c042403bb0b779e8ff3
saimon289 <37207504+saimon289@users.noreply.github.com>
2019-01-23 06:32:32 +0600

    sdafsdf
    sdfsfs

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('.your-class').slick({
+   slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+
+   dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
+
+    dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+
+  });
+});
